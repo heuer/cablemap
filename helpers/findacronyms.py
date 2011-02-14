b9c14b18d238e2dce5904c9d9629993285c60103
Lars <heuer@semagia.com>
2011-02-14 11:12:10 +0100

    Write acronyms to a text file

--- helpers/findacronyms.py
+++ helpers/findacronyms.py
@@ -52,13 +52,17 @@ def find_acronyms(in_dir):
     return s
     
 if __name__ == '__main__':
-    import os
+    import os, codecs
     if not os.path.isdir('./cable/'):
         raise Exception('Expected a directory "cable"')
-    current_acronyms = set([u'(INCSR)', u'AA/S', u'ABD', u'ADC', u'AEC', u'AESA', u'AF-PAK', u'AFM', u'AG', u'AK', u'AKP', u'AL', u'ALBA', u'AMA', u'AML/CFT', u'ANA', u'APHSCT', u'AQAP', u'AQIM', u'ARENA', u'ARS', u'ASD', u'ASD/ISA', u'AT', u'AU', u'BBC', u'BP', u'BR-163', u'BR-3', u'CAR', u'CCID', u'CDA', u'CDR', u'CEN-SAD', u'CENTCOM', u'CFE', u'CG', u'CIA', u'CISMOA', u'CITES', u'CIWG', u'CJCS', u'CMC', u'CN', u'CNP', u'COAS', u'CODEL', u'COGAT', u'COP-15', u'CPC', u'CSI', u'CSIS', u'CSU', u'CT', u'CTJWG', u'CW', u'CWS/BWC', u'DAS', u'DASD', u'DCA', u'DCM', u'DCOS', u'DDR', u'DEA', u'DFID', u'DG', u'DHS', u'DIO', u'DOL', u'DPRK', u'DRC', u'E-PINE', u'EAC', u'EC', u'EDF', u'EFCC', u'EFTA', u'EGIS', u'EITI', u'ELN', u'ENR', u'ETA', u'ETIA', u'EU', u'EU/US', u'EUR', u'EXBS', u'F-X', u'FARC', u'FATF', u'FBI', u'FCO', u'FCPA', u'FDLR', u'FDP', u'FM', u'FMLN', u'FTAA', u'FX2', u'G/TIP', u'GAERC', u'GBRV', u'GC', u'GDRC', u'GFA', u'GHQ', u'GICNT', u'GM', u'GOAJ', u'GOB', u'GOC', u'GOE', u'GOF', u'GOI', u'GOK', u'GOL', u'GOP', u'GOS', u'GOU', u'GPC', u'GSI', u'GSL', u'GSP', u'GTMO', u'HEU', u'HEU-LEU', u'HLDG', u'HLG', u'HMG', u'IAEA', u'IAF', u'ICAO', u'ICC', u'ICG-G', u'ICJ', u'ICRC', u'ICTY', u'IDF', u'II', u'III', u'ILSA', u'IMF', u'IMO', u'INR/B', u'IPCC', u'IPR', u'IRA', u'IRENA', u'IRGC', u'IRGC-QF', u'IRI', u'IRISL', u'ISA', u'ISAF', u'ISN/CTR', u'ITGA', u'JCET', u'JEM', u'JHA', u'JPMG', u'JSF', u'JUD', u'LAAD', u'LIFG', u'M/V', u'MANPADS', u'MB', u'MDC', u'MEA', u'MEK', u'MEP', u'MFA', u'MI-17', u'MI-6', u'MINUSTAH', u'MOA', u'MOD', u'MOIS', u'MONUC', u'MOP-3', u'MP', u'MRE', u'MST', u'MTCR', u'NAM', u'NATO', u'NCP', u'NDP', u'NEA', u'NEA/MAG', u'NEC', u'NFIU', u'NGO', u'NIE', u'NNSA', u'NPT', u'NSA', u'NSC', u'NSG', u'NTM-I', u'OAS', u'OECD', u'OEF', u'OIC', u'ONA', u'ONDCP', u'OPCON', u'OSCE', u'PAD', u'PAG', u'PASF', u'PCC', u'PD', u'PDAS', u'PGA', u'PJD', u'PM', u'PMDB', u'PNG', u'POC', u'PPP', u'PRC', u'PRC/DPRK', u'PRT', u'PS', u'PSDB', u'QATAR', u'QDR', u'QIZ', u'QME', u'RAK', u'RFG', u'RMB', u'ROK', u'RPO', u'RSO', u'RWE', u'S/CRS', u'S/CT', u'S/GC', u'S/SRAP', u'S/WCI', u'SAFE', u'SBIG', u'SCOFCAH', u'SCSL', u'SEDENA', u'SEGPRES', u'SG', u'SHIG', u'SLA', u'SLA/U', u'SLV', u'SOCAR', u'SPD', u'SR', u'SRSG', u'STAFFDEL', u'SWIFT', u'TBA', u'TFG', u'TFTP', u'TFTP/SWIFT', u'TIP', u'TSA', u'TSCC', u'TSCTP', u'U.S.-EU', u'U.S.-UK', u'U.S./UK', u'U/SYG', u'UAE', u'UAV', u'UDF', u'UK', u'UMP', u'UN', u'UNCHR', u'UNESCO', u'UNFCCC', u'UNGA', u'UNHCR', u'UNIFIL', u'UNODC', u'UNSC', u'UNSCOL', u'UNSCR', u'UPR', u'US', u'US-CU', u'US-EU', u'US-ROYG', u'USDP', u'USEB', u'USG', u'USTR', u'V/FM', u'VARIG', u'VFM', u'VI', u'VOA', u'VP', u'WEF', u'WTO', u'XVI', u'ZANU-PF'])
+    f = codecs.open('./acronyms.txt', 'rb', 'utf-8')
+    current_acronyms = set((l for l in f))
     acronyms = find_acronyms('./cable/')
     diff = acronyms ^ current_acronyms
     if diff:
         print 'difference: ', diff
-        print sorted(acronyms)
+        f = codecs.open('./acronyms.txt', 'wb', 'utf-8')
+        for acronym in sorted(acronyms):
+            f.write(acronym)
+            f.write('\n')
 

9b5ccae17e6de7e0dc24de2215dd7a96c1d3591d
Lars Heuer <heuer@semagia.com>
2011-02-14 17:51:55 +0100

    Added special case, added test Closes #5.

--- cablemap.core/cablemap/core/reader.py
+++ cablemap.core/cablemap/core/reader.py
@@ -105,6 +105,10 @@ D. 08 CAIRO 783
 E. 07 CAIRO 3214
 F. 07 CAIRO 2845
 """),
+    '06SAOPAULO348':
+        ('''BR
+\(B\) Sao Paulo 215;''', u'''BR
+REF: (B) Sao Paulo 215;''')
 }
 
 logger = logging.getLogger('cablemap-reader')

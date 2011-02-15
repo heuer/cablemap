4335ce36a8d8ffb1a30264312a072455e0181cb3
heuer <heuer@semagia.com>
2011-02-15 09:46:54 +0100

    Report the cable's path

--- helpers/find_malformedcableids.py
+++ helpers/find_malformedcableids.py
@@ -7,20 +7,23 @@ import re
 from cablemap.core.constants import REFERENCE_ID_PATTERN, MALFORMED_CABLE_IDS
 
 def find_malformed_ids(in_dir):
-    s = set()
+    dct = {}
     for root, dirs, files in os.walk(in_dir):
         for name in (n for n in files if '.html' in n):
-            name = name[:name.rindex('.')]
-            if not REFERENCE_ID_PATTERN.match(name):
-                s.add(name)
-    return s
+            reference_id = name[:name.rindex('.')]
+            if not REFERENCE_ID_PATTERN.match(reference_id):
+                dct[reference_id] = os.path.join(root, name)
+    return dct
     
 if __name__ == '__main__':
     import os, codecs
     if not os.path.isdir('./cable/'):
         raise Exception('Expected a directory "cable"')
     current = set(MALFORMED_CABLE_IDS.keys())
-    s = find_malformed_ids('./cable/')
+    dct = find_malformed_ids('./cable/')
+    s = set(dct.keys())
     diff = s ^ current
     if diff:
-        print 'difference: ', diff
+        print('difference: %r' % diff)
+        for ref in diff:
+            print('%s: %s' % (ref, dct.get(ref)))

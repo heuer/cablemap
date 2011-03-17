dca978d2d92dca18e5b6b83685bd4f0446e48f80
heuer <heuer@semagia.com>
2011-03-17 21:38:52 +0100

    Fixed docs

--- cablemap.core/tests/test_utils_titlefy.py
+++ cablemap.core/tests/test_utils_titlefy.py
@@ -32,7 +32,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 """\
-Tests cablemap.core.utils.subjectify
+Tests cablemap.core.utils.titlefy
 
 :author:       Lars Heuer (heuer[at]semagia.com)
 :organization: Semagia - <http://www.semagia.com/>

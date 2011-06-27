017a7c375852e8cd42cdac39ad0a3a563a8bd6f4
Lars Heuer <heuer@semagia.com>
2011-06-27 17:14:19 +0200

    Removed IRAN from allowed acronyms

--- helpers/update_helpers.py
+++ helpers/update_helpers.py
@@ -50,7 +50,7 @@ _ACRONYMS = (u'AA/S', u'ADC', u'AFM', u'AG', u'ASD/ISA', u'AU', u'AK', u'APHSCT'
              u'XVI',
              u'ZANU-PF')
 
-_UNWANTED = (u'SAVE', u'CITES', u'SHARIA')
+_UNWANTED = (u'SAVE', u'CITES', u'SHARIA', u'IRAN')
 
 def run_update(in_dir, predicate=None):
     acronyms = set(_ACRONYMS)

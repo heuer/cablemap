1e3ba67a65c11fc824246c1c98ed7d5eead7789a
heuer <heuer@semagia.com>
2011-07-09 00:14:07 +0200

    Removed CAN from acronyms

--- helpers/update_helpers.py
+++ helpers/update_helpers.py
@@ -51,7 +51,7 @@ _ACRONYMS = (u'AA/S', u'ADC', u'AFM', u'AG', u'ASD/ISA', u'AU', u'AK', u'APHSCT'
              u'XVI',
              u'ZANU-PF')
 
-_UNWANTED = (u'SAVE', u'CITES', u'SHARIA', u'IRAN', u'WHO')
+_UNWANTED = (u'SAVE', u'CITES', u'SHARIA', u'IRAN', u'WHO', u'CAN')
 
 def run_update(in_dir, predicate=None):
     acronyms = set(_ACRONYMS)

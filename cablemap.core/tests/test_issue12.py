0b4e463e67062aa7003a385b2ada3e3856104875
Lars <heuer@semagia.com>
2011-07-18 20:51:25 +0200

    Fixed test case, using the introduced canonical_id

--- cablemap.core/tests/test_issue12.py
+++ cablemap.core/tests/test_issue12.py
@@ -46,17 +46,21 @@ def test_issue12_manama():
     cable = cable_by_id('08MANAMA492')
     eq_(u'VZCZCXRO5008', cable.transmission_id)
     eq_(u'08MANAMA492', cable.reference_id)
+    eq_(u'08MANAMA492', cable.canonical_id)
     cable = cable_by_id('08ECTION01OF02MANAMA492')
     eq_(u'VZCZCXRO4772', cable.transmission_id)
-    eq_(u'08MANAMA492', cable.reference_id)
+    eq_(u'08ECTION01OF02MANAMA492', cable.reference_id)
+    eq_(u'08MANAMA492', cable.canonical_id)
 
 def test_issue12_saopaulo():
     cable = cable_by_id('08SAOPAULO335')
     eq_(u'VZCZCXRO3989', cable.transmission_id)
     eq_(u'08SAOPAULO335', cable.reference_id)
+    eq_(u'08SAOPAULO335', cable.canonical_id)
     cable = cable_by_id('08SCTION02OF02SAOPAULO335')
     eq_(u'VZCZCXRO3727', cable.transmission_id)
-    eq_(u'08SAOPAULO335', cable.reference_id)
+    eq_('08SCTION02OF02SAOPAULO335', cable.reference_id)
+    eq_(u'08SAOPAULO335', cable.canonical_id)
 
 
 if __name__ == '__main__':

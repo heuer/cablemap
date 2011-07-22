60f3a83c69287801aecf7f0d962ec24c54daac84
heuer <heuer@semagia.com>
2011-07-23 01:52:23 +0200

    Added USUN c14n

--- cablemap.core/cablemap/core/reader.py
+++ cablemap.core/cablemap/core/reader.py
@@ -183,6 +183,8 @@ def canonicalize_id(reference_id):
         return reference_id.replace(u'SANJSE', u'SANJOSE')
     elif u'USEU' in reference_id:
         return reference_id.replace(u'USEU', u'BRUSSELS')
+    elif u'USUN' in reference_id and not u'USUNNEWYORK' in reference_id:
+        return reference_id.replace(u'USUN', u'USUNNEWYORK')
     return MALFORMED_CABLE_IDS.get(reference_id, INVALID_CABLE_IDS.get(reference_id, reference_id))
 
 _REFERENCE_ID_FROM_HTML_PATTERN = re.compile('<h3>Viewing cable ([0-9]{2}[A-Z]+[A-Z0-9]+),', re.UNICODE)

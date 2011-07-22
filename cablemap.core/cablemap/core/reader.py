dd84f670da629516d004478c8daa321b1b185ef3
heuer <heuer@semagia.com>
2011-07-23 01:39:57 +0200

    Added support for references like those found in "01VATICAN5095"

--- cablemap.core/cablemap/core/reader.py
+++ cablemap.core/cablemap/core/reader.py
@@ -655,7 +655,7 @@ def parse_nondisclosure_deadline(content):
 
 _REF_START_PATTERN = re.compile(r'(?:[\nPROGRAM ]*REF|REF\(S\):?\s*)([^\n]+(\n\s*[0-9]+[,\s]+[^\n]+)?)', re.IGNORECASE|re.UNICODE)
 _REF_LAST_REF_PATTERN = re.compile(r'(\n[^\n]*\n)|(\n?[ ]*[A-Z](?:\.(?!O\.|S\.)|\))[^\n]+)', re.IGNORECASE|re.UNICODE)
-_REF_PATTERN = re.compile(r'(?:[A-Z](?:\.|\)|:)\s*)?([0-9]{2,4})?(?:\s*)([A-Z ]*[A-Z ]*[A-Z\-]{2,})(?:\s+)([0-9]+)(?:\s+\(([0-9]{2,4})\))?', re.MULTILINE|re.UNICODE|re.IGNORECASE)
+_REF_PATTERN = re.compile(r'(?:[A-Z](?:\.|\)|:)\s*)?\(?([0-9]{2,4})?\)?(?:\s*)([A-Z ]*[A-Z ]*[A-Z\-]{2,})(?:\s+)([0-9]+)(?:\s+\(([0-9]{2,4})\))?', re.MULTILINE|re.UNICODE|re.IGNORECASE)
 _REF_NOT_REF_PATTERN = re.compile(r'\n[0-9]\.[ ]*(?:\([A-Z]+\))?', re.IGNORECASE|re.UNICODE)
 _REF_STOP_PATTERN = re.compile('(classified by)|summary', re.IGNORECASE|re.UNICODE)
 #TODO: The following works for all references which contain something like 02ROME1196, check with other cables

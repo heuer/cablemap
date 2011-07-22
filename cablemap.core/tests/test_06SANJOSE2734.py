00c21d269ba97206e8c9d0b90169ffca8597f5cd
heuer <heuer@semagia.com>
2011-07-23 01:10:50 +0200

    Added SANJSE -> SANJOSE c14n tests

--- cablemap.core/tests/test_06SANJOSE2734.py
+++ cablemap.core/tests/test_06SANJOSE2734.py
@@ -32,50 +32,20 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 """\
-Tests identifier c14n parsing.
+Test against 06SANJOSE2734.
 
 :author:       Lars Heuer (heuer[at]semagia.com)
 :organization: Semagia - <http://www.semagia.com/>
 :license:      BSD license
 """
 from nose.tools import eq_
-from cablemap.core.constants import INVALID_CABLE_IDS, MALFORMED_CABLE_IDS
-from cablemap.core.reader import canonicalize_id
+from cablemap.core import cable_by_id
 
-_TEST_DATA = (
-    # input reference, expected id c14n
-    (u'05RIO123', u'05RIODEJANEIRO123'),
-    (u'05RIODEJAN123', u'05RIODEJANEIRO123'),
-    (u'05PARISFR1', u'05UNESCOPARISFR1'),
-    (u'05USUNESCOPARISFR1', u'05UNESCOPARISFR1'),
-    (u'05UNVIE1', u'05UNVIEVIENNA1'),
-    (u'05EMBASSYVIENNA1', u'05VIENNA1'),
-    (u'05EMBASSYBLABLA1', u'05BLABLA1'),
-    (u'06SECSTATE123', u'06STATE123'),
-    (u'06SECDEF123', u'06SECDEF123'),
-    (u'06PORT-OF-SPAIN123', u'06PORTOFSPAIN123'),
-    (u'07PORT-AU-PRINCE12', u'07PORTAUPRINCE12'),
-    (u'07PAUP12', u'07PORTAUPRINCE12'),
-    (u'07SANJSE12', u'07SANJOSE12'),
-)
 
-def test_c14n():
-    def check(reference_id, canonical_id):
-        eq_(canonical_id, canonicalize_id(reference_id))
-    for r, c in _TEST_DATA:
-        yield check, r, c
-
-def test_c14n_malformed_ids():
-    def check(reference_id, canonical_id):
-        eq_(canonical_id, canonicalize_id(reference_id))
-    for r, c in MALFORMED_CABLE_IDS.iteritems():
-        yield check, r, c
-
-def test_c14n_illegal_ids():
-    def check(reference_id, canonical_id):
-        eq_(canonical_id, canonicalize_id(reference_id))
-    for r, c in INVALID_CABLE_IDS.iteritems():
-        yield check, r, c
+def test_06SANJOSE2734():
+    cable = cable_by_id('06SANJOSE2734')
+    assert cable
+    eq_([u'06STATE19152', u'06SANJOSE2223', u'06SANJOSE1955'], cable.references)
 
 
 if __name__ == '__main__':

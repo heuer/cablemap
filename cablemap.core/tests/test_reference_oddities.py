0f007cf442d9dad9ec5306b5be0c32af49b46922
heuer <heuer@semagia.com>
2011-07-23 11:21:45 +0200

    Merged test cases into one file

--- cablemap.core/tests/test_reference_oddities.py
+++ cablemap.core/tests/test_reference_oddities.py
@@ -32,7 +32,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 """\
-Test against 06SANJOSE2734.
+Several REF parsing tests.
 
 :author:       Lars Heuer (heuer[at]semagia.com)
 :organization: Semagia - <http://www.semagia.com/>
@@ -41,13 +41,28 @@ Test against 06SANJOSE2734.
 from nose.tools import eq_
 from cablemap.core import cable_by_id
 
+_TEST_DATA = (
+        (u'01VATICAN5095', [u'99ROME2196']),
+        ('04THEHAGUE1919', [u'04BRUSSELS3226']),
+        ('05BRASILIA1851', [u'05STATE127529', u'05STATE110840', u'05USUNNEWYORK1442']),
+        ('05PORTAUPRINCE1609', [u'05PORTAUPRINCE1537', u'05TOKYO3020', u'05OSLO868', u'05OTTAWA1734']),
+        ('06PORTAUPRINCE2187', [u'06PORTAUPRINCE1734', u'06PORTAUPRINCE1561']),
+        ('06SANJOSE2734', [u'06STATE19152', u'06SANJOSE2223', u'06SANJOSE1955']),
+        ('07LIMA2129', [u'07LIMA1841', u'07LIMA2000', u'07LIMA2009', u'07LIMA2026']),
+        ('07LIMA2323', [u'07LIMA2000', u'07LIMA2009', u'07LIMA2126', u'07LIMA2236']),
+        
+        
+    )
 
-def test_06SANJOSE2734():
-    cable = cable_by_id('06SANJOSE2734')
-    assert cable
-    eq_([u'06STATE19152', u'06SANJOSE2223', u'06SANJOSE1955'], cable.references)
 
-
+def test_references():
+    def check(cable_id, references):
+        cable = cable_by_id(cable_id)
+        assert cable
+        eq_(references, cable.references)
+    for cable_id, refs in _TEST_DATA:
+        yield check, cable_id, refs
+    
 if __name__ == '__main__':
     import nose
     nose.core.runmodule()

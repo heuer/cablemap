dc3aceaa1891447d6a312068b492f8927a776d41
heuer <heuer@semagia.com>
2011-08-04 12:56:07 +0200

    Fixed typo

--- cablemap.core/cablemap/core/models.py
+++ cablemap.core/cablemap/core/models.py
@@ -117,7 +117,7 @@ class Reference(tuple):
     __slots__ = ()
     
     def __new__(cls, value, kind, name=None, title=None):
-        return tuple.__new__(cls, (value, kind, name.upper() if name else None), title)
+        return tuple.__new__(cls, (value, kind, name.upper() if name else None, title)
 
     def is_cable(self):
         return self.kind == consts.REF_KIND_CABLE

68adcd8ffef7c48933f78b03183028aabe040f06
heuer <heuer@semagia.com>
2011-08-21 22:26:49 +0200

    Simplified code

--- helpers/generate_topicmaps.py
+++ helpers/generate_topicmaps.py
@@ -4,7 +4,7 @@ Experimental script to generarte topic maps.
 """
 import os
 import codecs
-from cablemap.core.handler import DefaultMetadataOnlyFilter, MultipleCableHandler, DelegatingCableHandler, handle_directory
+from cablemap.core.handler import DefaultMetadataOnlyFilter, DebitlyFilter, TeeCableHandler, MultipleCableHandler, DelegatingCableHandler, handle_directory
 from cablemap.tm.handler import create_ctm_handler, create_xtm_handler
 
 import logging 
@@ -40,20 +40,20 @@ def openfile(name):
     return open(os.path.join(os.path.dirname(__file__), name), 'wb')
 
 def generate_topicmaps(cable_directory):
-    ctm_tm = openfile('cables.ctm')
-    xtm_tm = openfile('cables.xtm')
-    slos_ctm = openfile('cable-subject-locators.ctm')
-    slos_xtm = openfile('cable-subject-locators.xtm')
-    content_ctm = openfile('cable-content.ctm')
-    content_xtm = openfile('cable-content.xtm')
-    files = [ctm_tm, xtm_tm, slos_ctm, slos_xtm, content_ctm, content_xtm]
+    def tee(files, filename):
+        """\
+
+        """
+        ctm = openfile(filename + '.ctm')
+        xtm = openfile(filename + '.xtm')
+        files.append(ctm)
+        files.append(xtm)
+        return TeeCableHandler(create_ctm_handler(ctm), create_xtm_handler(xtm))
+    files = []
     handlers = []
-    handlers.append(DefaultMetadataOnlyFilter(create_ctm_handler(ctm_tm)))
-    handlers.append(DefaultMetadataOnlyFilter(create_xtm_handler(xtm_tm)))
-    handlers.append(SubjectLocatorsCableHandler(create_ctm_handler(slos_ctm)))
-    handlers.append(SubjectLocatorsCableHandler(create_xtm_handler(slos_xtm)))
-    handlers.append(ContentCableHandler(create_ctm_handler(content_ctm)))
-    handlers.append(ContentCableHandler(create_xtm_handler(content_xtm)))
+    handlers.append(DefaultMetadataOnlyFilter(DebitlyFilter(tee(files, 'cables'))))
+    handlers.append(SubjectLocatorsCableHandler(tee(files, 'cable-subject-locators')))
+    handlers.append(ContentCableHandler(tee(files, 'cable-content')))
     handle_directory(cable_directory, MultipleCableHandler(handlers))
     for f in files:
         f.close()

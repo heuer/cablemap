569d52179cbe0b65476d1a5b43438fbf71b30cb2
heuer <heuer@semagia.com>
2011-08-22 09:14:38 +0200

    Cache resolved bit.ly IRIs

--- cablemap.core/cablemap/core/handler.py
+++ cablemap.core/cablemap/core/handler.py
@@ -176,20 +176,33 @@ class DebitlyFilter(DelegatingCableHandler):
     """\
     `DelegatingCableHandler` implementation that expands bit.ly media IRIs
     """
+    def __init__(self, handler):
+        """\
+
+        `handler`
+            The ICableHandler which should receive the (filtered) events.
+        """
+        super(DebitlyFilter, self).__init__(handler)
+        self._bitly2url = {}
+        # For some reason this returns 404, acc. to <http://knowurl.com/>
+        # the IRI is:
+        self._bitly2url[u'http://bit.ly/mDfYBE'] = u'http://www.haiti-liberte.com/archives/volume4-46/Les%20c%C3%A2bles%20de%20WikiLeaks%20sur%20Ha%C3%AFti%20publi%C3%A9s%20par%20Ha%C3%AFti%20Libert%C3%A9.asp'
+
     def handle_media_iri(self, iri):
         class HeadRequest(urllib2.Request):
             def get_method(self):
                 return 'HEAD'
         if iri.startswith(u'http://bit.ly'):
-            if iri == u'http://bit.ly/mDfYBE':
-                # For some reason this returns 404, acc. to <http://knowurl.com/>
-                # the IRI is:
-                iri = u'http://www.haiti-liberte.com/archives/volume4-46/Les%20c%C3%A2bles%20de%20WikiLeaks%20sur%20Ha%C3%AFti%20publi%C3%A9s%20par%20Ha%C3%AFti%20Libert%C3%A9.asp'
+            url = self._bitly2url.get(iri)
+            if url:
+                iri = url
             else:
                 request = HeadRequest(iri)
                 try:
                     response = urllib2.urlopen(request)
-                    iri = response.geturl()
+                    url = response.geturl()
+                    self._bitly2url[iri] = url
+                    iri = url
                 except urllib2.HTTPError:
                     pass
         self._handler.handle_media_iri(iri)

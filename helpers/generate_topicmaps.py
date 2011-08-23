f3c29bf9dbf1fdb202e297f5897b8856db433455
heuer <heuer@semagia.com>
2011-08-23 16:22:28 +0200

    Added media coverage topic map (disabled by default)

--- helpers/generate_topicmaps.py
+++ helpers/generate_topicmaps.py
@@ -4,8 +4,11 @@ Experimental script to generarte topic maps.
 """
 import os
 import codecs
+from tm.mio import handler
+from mio.ctm.miohandler import CTMHandler
+from mio.xtm.miohandler import XTM21Handler
 from cablemap.core.handler import DefaultMetadataOnlyFilter, DebitlyFilter, TeeCableHandler, MultipleCableHandler, DelegatingCableHandler, handle_directory
-from cablemap.tm.handler import create_ctm_handler, create_xtm_handler
+from cablemap.tm.handler import create_ctm_handler, create_xtm_handler, create_ctm_miohandler, create_xtm_miohandler, MediaTitleResolver
 
 import logging 
 import sys
@@ -36,10 +39,16 @@ class ContentCableHandler(DelegatingCableHandler):
         return getattr(self._handler, name)
 
 
+def mediatitle_resolver_ctm(fileobj):
+    return MediaTitleResolver(CTMHandler(fileobj))
+
+def mediatitle_resolver_xtm(fileobj):
+    return MediaTitleResolver(XTM21Handler(fileobj=fileobj))
+
 def openfile(name):
     return open(os.path.join(os.path.dirname(__file__), name), 'wb')
 
-def generate_topicmaps(cable_directory):
+def generate_topicmaps(cable_directory, handle_media=False):
     def tee(files, filename):
         ctm = openfile(filename + '.ctm')
         xtm = openfile(filename + '.xtm')
@@ -51,11 +60,18 @@ def generate_topicmaps(cable_directory):
     handlers.append(DefaultMetadataOnlyFilter(DebitlyFilter(tee(files, 'cables'))))
     handlers.append(SubjectLocatorsCableHandler(tee(files, 'cable-subject-locators')))
     handlers.append(ContentCableHandler(tee(files, 'cable-content')))
+    if handle_media:
+        ctm, xtm = openfile('media-iris.ctm'), openfile('media-iris.xtm')
+        files.append(ctm)
+        files.append(xtm)
+        h = DebitlyFilter(MediaTitleResolver(handler.TeeMapHandler(create_ctm_miohandler(ctm), create_xtm_miohandler(xtm))))
+        handlers.append(h)
     handle_directory(cable_directory, MultipleCableHandler(handlers))
     for f in files:
         f.close()
-    
-    
+
+
+   
 if __name__ == '__main__':
     if not os.path.isdir('./cable/'):
         raise Exception('Expected a directory "cable"')

7ed1add4fd9cb118e803f13c1cf1f8cb5cfb2e4d
heuer <heuer@semagia.com>
2011-09-19 11:51:52 +0200

    Updated acronyms

--- cablemap.core/cablemap/core/utils.py
+++ cablemap.core/cablemap/core/utils.py
@@ -333,6 +333,7 @@ _SPECIAL_WORDS = {
     'SECGEN': u'SecGen',
     'Ex-IM': u'Ex-IM',
     'GOH': u'GoH',
+    'US-LIBYAN': u'US-Libyan',
 }
 
 _TITLEFY_SMALL_PATTERN = re.compile(r'^(([0-9]+(th|st|rd|nd))|(a)|(an)|(and)|(as)|(at)|(but)|(by)|(en)|(for)|(if)|(in)|(of)|(on)|(or)|(the)|(to)|(v\.?)|(via)|(vs\.?))$', re.IGNORECASE)

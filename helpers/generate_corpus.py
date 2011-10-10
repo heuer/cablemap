8c588b0e1cb207f983dce345d1e682d65a3c7149
Lars <heuer@semagia.com>
2011-10-11 00:24:09 +0200

    Applied changes from the .nlp package

--- helpers/generate_corpus.py
+++ helpers/generate_corpus.py
@@ -5,11 +5,11 @@ Playground to generarte the cable corpus
 import os
 from cablemap.core import handle_source, predicates as pred
 from cablemap.core.handler import DefaultMetadataOnlyFilter, TeeCableHandler
-from cablemap.nlp.handler import CorpusWriter
+from cablemap.nlp.handler import CorpusHandler
 
 def generate_corpus(src):
-    handler = TeeCableHandler(DefaultMetadataOnlyFilter(CorpusWriter('./', prefix='german_cables_metadata_')),
-                              CorpusWriter('./', prefix='german_cables_'))
+    handler = TeeCableHandler(DefaultMetadataOnlyFilter(CorpusHandler('./', prefix='german_cables_metadata_')),
+                              CorpusHandler('./', prefix='german_cables_'))
     handle_source(src, handler, pred.origin_filter(pred.origin_germany))
 
 if __name__ == '__main__':

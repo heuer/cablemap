6a3b534538b3ec22c9af141ce76f7202de73f9c3
Lars <heuer@semagia.com>
2015-02-09 10:12:06 +0100

    Simplified file header, updated CHANGES

--- cablemap.core/tests/test_malformed_ids.py
+++ cablemap.core/tests/test_malformed_ids.py
@@ -1,35 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2011 - 2012 -- Lars Heuer <heuer[at]semagia.com>
+# Copyright (c) 2011 - 2015 -- Lars Heuer <heuer[at]semagia.com>
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-#     * Neither the project name nor the names of the contributors may be 
-#       used to endorse or promote products derived from this software 
-#       without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# License: BSD, see LICENSE.txt for more details.
 #
 """\
 Tests the handling of malformed WikiLeaks cable ids.
@@ -42,8 +16,10 @@ from nose.tools import ok_, eq_
 from cablemap.core import cable_by_id, c14n
 from cablemap.core.constants import MALFORMED_CABLE_IDS
 
+
 def _get_test_cases():
     return MALFORMED_CABLE_IDS.iteritems()
+
 
 def test_from_malformed_cable_id():
     def check(incorrect_id, correct_id):
@@ -54,6 +30,7 @@ def test_from_malformed_cable_id():
         eq_(c14n.canonicalize_id(correct_id), cable.canonical_id, 'Unexpected canonical identifier for the correct id')
     for incorrect_id, correct_id in _get_test_cases():
         yield check, incorrect_id, correct_id
+
 
 def test_to_malformed_cable_id():
     def check(correct_id):
